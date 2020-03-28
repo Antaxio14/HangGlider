0094cd9da35312a2cb4d84ed572b9a241cfb3141
Invy55 <38048104+Invy55@users.noreply.github.com>
2020-03-28 11:10:43 +0100

    Big, and I mean VERY big code clean-up

--- src/Glider/HangGlider.php
+++ src/Glider/HangGlider.php
@@ -1,78 +1,58 @@
 <?php
-  
+
 namespace AntaxioYT\HangGlider;
 
-use pocketmine\event\Listener;
-
-use pocketmine\Server;
-use pocketmine\plugin\PluginBase;
 use pocketmine\Player;
+use pocketmine\Server;
 use pocketmine\item\Item;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat;
-use pocketmine\entity\Attribute;
-use pocketmine\event\player\PlayerItemHeldEvent;
 use pocketmine\entity\Effect;
-use pocketmine\entity\EffectInstance;
 use pocketmine\entity\Entity;
+use pocketmine\entity\Attribute;
+use pocketmine\entity\EffectInstance;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerItemHeldEvent;
 
-class HangGlider extends PluginBase implements Listener
-{
+class HangGlider extends PluginBase implements Listener {
 
-    public function onEnable()
-    {
+    public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-}
-
+    }
  
-    public function ItemHeld(PlayerItemHeldEvent $event)
-{
+    public function ItemHeld(PlayerItemHeldEvent $event){
         $player = $event->getPlayer();
         $item = $event->getItem();
         if($item->getId() == Item::SLIME_BALL){
-
             $eff = new EffectInstance(Effect::getEffect(Effect::LEVITATION), 100 * 99999, -4, true);
             $player->addEffect($eff);
+        } else if (($player->hasEffect(Effect::LEVITATION)) and (!$item->getId() == Item::SLIME_BALL)){
+            $player->removeEffect(24);
+        } else {
+            $player->removeEffect(24);
+        } 
+    }
 
-   } else if (($player->hasEffect(Effect::LEVITATION)) and (!$item->getId() == Item::SLIME_BALL)){
+    public function onDamage(EntityDamageEvent $event){
+        $player = $event->getEntity();
+        if($player instanceof Player){
+            if($event->getEntity()->getInventory()->getItemInHand()->getId() === 341){
+                if ($event->getCause() === EntityDamageEvent::CAUSE_FALL){
+                    $event->setCancelled();
+    
+                }
+            }
+        }
+    } 
 
-    $player->removeEffect(24);
-   
-  } else {
-        $player->removeEffect(24);
-
- } 
-   }
-
-public function onDamage(EntityDamageEvent $event){
-
-              $player = $event->getEntity();
-              
-                       if($player instanceof Player){
- if($event->getEntity()->getInventory()->getItemInHand()->getId() === 341){
-if ($event->getCause() === EntityDamageEvent::CAUSE_FALL){
-
-        $event->setCancelled();
- 
-} else {
-} 
-} else {
-} 
-} else {
-} 
-} 
-
-
-
-public function Leave(PlayerQuitEvent $event){
-          $player = $event->getPlayer();
+    public function Leave(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
         $item = $player->getInventory()->getItemInHand();
         if($item->getId() == Item::SLIME_BALL){
-                $player->removeEffect(24);
- }
-}
-
+            $player->removeEffect(24);
+        }
+    }
 
 } 
